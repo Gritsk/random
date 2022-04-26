e0e4b52d1215e05daea40470fb32e873c9062512
Gritsk <43971741+Gritsk@users.noreply.github.com>
2022-04-26 20:54:30 +0100

    Update random.py

--- random.py
+++ random.py
@@ -21,3 +21,8 @@ for conversation in root:
                 nonpredatorylaceholder.write(attr.text)
             countNonPredatory += 1
         convid.clear()
+
+        
+ !pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+ !pip install mxnet-cu110
+  
